d723c3215281b579d3e5608336ea243e57ce7239
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-08-03 21:18:04 +0330

    first commit

---
+++ src/components/CoursesData.js
@@ -0,0 +1,7 @@
+let coursesData = [
+    {id:1 , title:'js course', price:300_000},
+    {id:2 , title:'React course', price:400_000},
+    {id:3 , title:'Next course', price:'free'}
+]
+
+export default coursesData

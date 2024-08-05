313b6e756b76c5e75d2f2de19a8b9a94cd68cafc
pouria-mobaraki <ss_pooria69@yahoo.com>
2024-08-05 13:29:13 +0330

    completed main course component

--- src/components/Course.js
+++ src/components/Course.js
@@ -8,12 +8,11 @@ export default function Course(props) {
       <Card style={{ width: '18rem' }}>
     <Card.Img variant="top" src={props.img} />
     <Card.Body>
-      <Card.Title>Card Title</Card.Title>
+      <Card.Title>{props.title}</Card.Title>
       <Card.Text>
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
+        {props.price.toLocaleString()}
       </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
+      <Button variant="primary">Get Course</Button>
     </Card.Body>
     </Card>
     </Link>
--- src/components/MainCourse.js
+++ src/components/MainCourse.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import CoursesData from '../CoursesData'
+import { Container,Row,Col } from 'react-bootstrap'
+
 
 export default function MainCourse() {
  let params = useParams()
  console.log(params.courseId);
+ let mainCourseData = CoursesData.find(course =>course.id == params.courseId)
+ console.log(mainCourseData);
  
 
   return (
-    <div>
-
-    </div>
+    <Container className='mt-5'>
+      <Row>
+        <Col>
+        <img src={mainCourseData.img} style={{width:'80%',height:'300px'}} alt=""/>
+        </Col>
+        <Col>
+        <h2>{mainCourseData.title}</h2>
+        <p>{mainCourseData.description}</p>
+        </Col>
+      </Row>
+    </Container>
   )
 }
